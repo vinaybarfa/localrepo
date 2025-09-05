75fee5ec243631a57d7b9f4ac931527c8d7640b4
vinaybarfa <vinaybarfa54@gmail.com>
2025-09-05 23:55:13 +0530

    Create class Thread1 which implement Runnable interface

---
+++ main/Thread/MyThread.java
@@ -0,0 +1,15 @@
+package LocalRepo.main.Thread;
+
+class Thread1 implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
+
+public class MyThread {
+    public static void main(String[] args) {
+
+    }
+}

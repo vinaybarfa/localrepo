b2e393f6ac66926d52d2e895ff2cf05aea19286e
vinaybarfa <vinaybarfa54@gmail.com>
2025-09-06 06:18:01 +0530

    call the run with help of start method
    add while loop

--- main/Thread/MyThread.java
+++ main/Thread/MyThread.java
@@ -2,9 +2,13 @@ package LocalRepo.main.Thread;
 
 class Thread1 implements Runnable{
 
+    int i = 0;
     @Override
     public void run() {
-        System.out.println("Thread is running");
+        while(i<200){
+            System.out.println("Thread is running");
+            i++;
+        }
     }
 }
 
@@ -12,6 +16,6 @@ public class MyThread {
     public static void main(String[] args) {
         Thread1 t1 = new Thread1();
         Thread t = new Thread(t1);
-        
+        t.start();
     }
 }

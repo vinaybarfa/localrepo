341b4050c5fbcfd07ff94c7f8ce3a5ab2c77f27a
vinaybarfa <vinaybarfa54@gmail.com>
2025-09-06 10:22:37 +0530

    add enum of city

--- main/Enums/City.java
+++ main/Enums/City.java
@@ -1,5 +1,9 @@
 package LocalRepo.main.Enums;
 
+enum city {
+    Indore, Bhopal, Jabalpur, Mandsor, Dhar, Nimach, Bairwani, Singhana, Jirabad, Kashvi, Ratlam,Jhabuaa, Amjera, Khargon, Borud  
+}
+
 
 public class City {
     public static void main(String[] args) {

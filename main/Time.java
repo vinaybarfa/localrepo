45dc56a41d8a99d7dc43745251f06fc830196405
vinaybarfa <vinaybarfa54@gmail.com>
2025-09-06 06:38:27 +0530

    create Thread class

---
+++ main/Time.java
@@ -0,0 +1,13 @@
+package LocalRepo.main;
+
+import java.time.LocalTime;
+
+public class Time {
+
+    public static void main(String[] args) {
+        LocalTime lt = LocalTime.now();
+        System.out.println(lt);
+    }
+    
+    
+}

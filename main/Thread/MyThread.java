269a7c9d23848c73b649dda56977f6ef1536f2de
vinaybarfa <vinaybarfa54@gmail.com>
2025-09-06 09:53:29 +0530

    add enum

--- main/Thread/MyThread.java
+++ main/Thread/MyThread.java
@@ -1,15 +1,35 @@
 package LocalRepo.main.Thread;
 
 class Thread1 implements Runnable{
-
+    
+    int i = 0;
     @Override
     public void run() {
-        System.out.println("Thread is running");
+        while(i<200){
+            System.out.println("Thread 1 is running");
+            i++;
+        }
+    }
+}
+class Thread2 extends Thread{
+    
+    int i = 0;
+    @Override
+    public void run() {
+        while(i<200){
+            System.out.println("Thread 2 is running");
+            i++;
+        }
     }
 }
 
+
 public class MyThread {
     public static void main(String[] args) {
-
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+        Thread t = new Thread(t1);
+        t.start();
+        t2.start();
     }
 }
